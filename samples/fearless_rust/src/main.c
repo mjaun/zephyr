7e97799dba89b686a8cd32f097530d8f27710e4b
Mario Jaun <mario.jaun@zuehlke.com>
2024-08-31 14:50:18 +0200

    go to deep sleep and repeat

--- samples/fearless_rust/src/main.c
+++ samples/fearless_rust/src/main.c
@@ -1,9 +1,15 @@
+#include <esp_sleep.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/poweroff.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
 #include "wifi.h"
 #include "udp.h"
+
+LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
@@ -20,12 +26,21 @@ int main(void)
 	const char* hello = "hello world!\n";
 	(void) udp_send(hello, strlen(hello));
 
+	// not sure how to properly wait until the UDP data has been sent
 	k_sleep(K_SECONDS(3));
 
 teardown_disconnect:
 	wifi_disconnect();
 
 teardown:
+	LOG_INF("Powering down");
+
+	while (log_process()) {
+		// flush log messages
+	}
+
+	esp_sleep_enable_timer_wakeup(10 * 1000000);
+	sys_poweroff();
 	return 0;
 
 	const struct device *dev = DEVICE_DT_GET(DT_ALIAS(temp_sensor));
