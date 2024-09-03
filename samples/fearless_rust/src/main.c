1d2aa1cc9d250e55eb71b971cff6f741479672ae
Mario Jaun <mario.jaun@zuehlke.com>
2024-09-03 15:26:01 +0200

    send json data

--- samples/fearless_rust/src/main.c
+++ samples/fearless_rust/src/main.c
@@ -21,8 +21,16 @@ int main(void)
 		goto teardown;
 	}
 
-	LOG_INF("Device ID: %016llx", measurement.device_id);
-	LOG_INF("Temperature: %.3f C", (double) measurement.value);
+	LOG_INF("Device ID:   %s", measurement.device_id);
+	LOG_INF("Temperature: %.3f C", (double) measurement.temperature);
+
+	char measurement_json[256];
+
+	if (measurement_serialize(&measurement, measurement_json, sizeof(measurement_json)) != 0) {
+		goto teardown;
+	}
+
+	LOG_INF("%s", measurement_json);
 
 	LOG_INF("Starting to upload data");
 
@@ -33,24 +41,18 @@ int main(void)
 	}
 
 	if (udp_init() != 0) {
-		goto teardown_disconnect;
+		goto disconnect;
 	}
 
-	const char* hello = "hello world!\n";
-	(void) udp_send(hello, strlen(hello));
+	(void) udp_send(measurement_json, strlen(measurement_json));
 
 	// not sure how to properly wait until the UDP data has been sent
 	k_sleep(K_SECONDS(3));
 
-teardown_disconnect:
+disconnect:
 	wifi_disconnect();
 
 teardown:
 	LOG_INF("Powering down");
-
-	while (log_process()) {
-		// flush log messages
-	}
-
 	sys_poweroff();
 }
--- samples/fearless_rust/src/measurement.h
+++ samples/fearless_rust/src/measurement.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdlib.h>
 
 struct measurement {
-    float value;
-    uint64_t device_id;
+    char device_id[16];
+    float temperature;
 };
 
 int measurement_get(struct measurement *output);
+int measurement_serialize(const struct measurement* measurement, char* buffer, size_t buffer_size);
